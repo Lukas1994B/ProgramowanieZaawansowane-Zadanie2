5ae00caa9cac841cbb9ed978af73b398b7335acc
Łukasz 109105 <chx87185@student.chorzow.wsb.pl>
2021-09-18 17:14:11 +0200

    Dodaj pliki projektów.

---
+++ AdvancedProgramming_Lesson2/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedProgramming_Lesson2.Models
+{
+    public class Movie
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+    }
+}
